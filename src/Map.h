c4a01e782a90721795d790ce1575ff7e44c6c4c8
_hyperion <u29562695@tuks.co.za>
2013-09-18 21:18:11 +0200

    Map preliminary datastructure and interface

--- src/Map.h
+++ src/Map.h
@@ -1,9 +1,20 @@
 #ifndef MAP_H
 #define MAP_H
+#include <vector>
+#include "Piece.h"
 
 class Map
 {
+public:
+	bool move(const int& fromX, const int& fromY, const int& toX, const int& toY);
+	void getSpriteRelativeCoord(int& x, int& y) const;//not as generic as it could be
+	void setPieceAt(const Piece* ,const int& x, const int& y);
+	Piece& getHandleAt(const int& x, const int& y);//might need to be a pointer, we'll see
+	
+	void runAction();
 
+private:
+	std::vector<std::vector<Piece*> > map;
 };
 
 #endif
