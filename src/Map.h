e3e2680ba5c26482ef5140ab9eb5597070192adf
Frank Allenby <fallenby@outlook.com>
2013-09-18 21:59:36 +0200

    Added proper indentation. Bad Hyperion.

--- src/Map.h
+++ src/Map.h
@@ -42,16 +42,16 @@ struct Coord : public std::pair<int, int>
 
 class Map
 {
-public:
-	bool move(const int& fromX, const int& fromY, const int& toX, const int& toY);
-	void getSpriteRelativeCoord(int& x, int& y) const;//not as generic as it could be
-	void setPieceAt(const Piece* ,const int& x, const int& y);
-	Piece& getHandleAt(const int& x, const int& y);//might need to be a pointer, we'll see
-	
-	void runAction();
+    public:
+        bool move(const int& fromX, const int& fromY, const int& toX, const int& toY);
+        void getSpriteRelativeCoord(int& x, int& y) const;//not as generic as it could be
+        void setPieceAt(const Piece* ,const int& x, const int& y);
+        Piece& getHandleAt(const int& x, const int& y);//might need to be a pointer, we'll see
 
-private:
-	std::vector<std::vector<Piece*> > map;
+        void runAction();
+
+    private:
+        std::vector<std::vector<Piece*> > map;
 };
 
 #endif
