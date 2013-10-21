eaac847f7e8fa76ec0e70e2712f2b5cf04d0b129
fallenby <fallenby@outlook.com>
2013-10-21 21:13:44 +0200

    Score and hp deduction changes.

--- src/Wall.cpp
+++ src/Wall.cpp
@@ -10,17 +10,18 @@ void Wall::iAttackedYou(Piece* const assailant, unsigned int& damage, Map* calle
 {
 	if (dynamic_cast<Sprite*>(assailant))
 	{
+		std::string attackMod = "attacks";
+
+		if (damage > assailant->getAttackPower())
+		{
+			attackMod = "critical attacks";
+		}
+		std::cout << "Player " << attackMod << ", dealing a total damage amount of " << ((damage > getCurrentLife()) ? getCurrentLife() : damage) << ".\n";
+
 		// Wall only applies the 10% of life damage penalty and score reduction
 		// if the attack actually hits. An attack value of zero indicates a miss.
 		if (damage > 0)
 		{
-			std::string attackMod = "attacks";
-
-			if (damage > assailant->getAttackPower())
-			{
-				attackMod = "critical attacks";
-			}
-			std::cout << "Player " << attackMod << ", dealing a total damage amount of " << ((damage > getCurrentLife()) ? getCurrentLife() : damage) << ".\n";
 			unsigned healthLoss = floor(assailant->getMaxLife() * 0.1);
 			static_cast<Sprite*>(assailant)->getOwner()->removeScore(healthLoss);
 			assailant->decreaseLife(healthLoss, caller);
